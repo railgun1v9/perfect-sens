ee25549e4e3fee1098b73494151e2f8aa5853479
railgun1v9 <tempestblaze21@gmail.com>
2020-10-27 01:17:15 -0400

    Working on top nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,25 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default function Header(props) {
-  return <h1>{props.headerText}</h1>
+const ListLink = props => (
+    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+        <Link to={props.to}>{props.children}</Link>
+    </li>
+)
+
+export default function Header() {
+    return (
+        <div style={ {margin: `3rem auto`, maxWidth: 650, padding: `0 1rem`}}>
+            <header style={{ marginBottom: `1.5rem`}}>
+                <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`}}>
+                    <h3 style={{ display: `inline` }}>MySweetSite</h3>
+                </Link>
+                <ul style={{ listStyle: `none`, float: `right` }}>
+                    <ListLink to="/">Home</ListLink>
+                    <ListLink to="/about/">About</ListLink>
+                    <ListLink to="/contact/">Contact</ListLink>
+                </ul>
+            </header>
+        </div>
+    )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color : `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <Header headerText="Hello Gatsby!" />
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <div>
+      <Header />
+      <Layout>
+        <h1>Hello Gatsby!</h1>
+        <p>What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+      </Layout>
     </div>
   );
 }
